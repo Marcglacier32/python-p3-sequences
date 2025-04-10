ce9c46276d189fc6cba93500741622b21504fdf7
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-04-10 14:41:18 +0300

    Implemented print_fibonacci in sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length <= 0:
+        print(fibonacci)
+        return
+    elif length == 1:
+        fibonacci.append(0)
+        print(fibonacci)
+        return
+    
+    fibonacci.append(0)
+    fibonacci.append(1)
+    
+    for i in range(2, length):
+        fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+    
+    print(fibonacci)
+    print_fibonacci(9) # Test it
